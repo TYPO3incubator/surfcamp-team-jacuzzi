0029c9165f4523c1e8073f1e45df917fbb4bd962
Oliver Bartsch <bo@cedev.de>
2024-04-09 12:45:44 +0200

    [TASK] Update import data and reset command

--- local_packages/psi/Classes/Command/ResetInitialisation.php
+++ local_packages/psi/Classes/Command/ResetInitialisation.php
@@ -9,7 +9,9 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use TYPO3\CMS\Core\Core\Environment;
 use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 #[AsCommand(
     name: 'psi:reset-initialisation',
@@ -29,12 +31,31 @@ class ResetInitialisation extends Command
         $count = $queryBuilder
             ->delete('sys_registry')
             ->where(
+                $queryBuilder->expr()->like('entry_namespace', $queryBuilder->createNamedParameter('extensionDataImport')),
                 $queryBuilder->expr()->like('entry_key', $queryBuilder->createNamedParameter('%jacuzzi/psi%'))
+            )
+            ->orWhere(
+                $queryBuilder->expr()->like('entry_namespace', $queryBuilder->createNamedParameter('siteConfigImport')),
+                $queryBuilder->expr()->like('entry_key', $queryBuilder->createNamedParameter('psi-intranet'))
             )
             ->executeStatement();
 
-        (new SymfonyStyle($input, $output))->info('Removed ' . $count . ' records.');
+        $io = new SymfonyStyle($input, $output);
 
-        return 0;
+        if ($count > 0) {
+            $io->info('Removed ' . $count . ' records.');
+        } else {
+            $io->warning('No records to remove found.');
+        }
+
+        $directory = Environment::getConfigPath() . '/sites/psi-intranet/';
+        if (!GeneralUtility::rmdir($directory, true)) {
+            $io->warning('Site configuration could not be removed.');
+            return Command::SUCCESS;
+        }
+
+        $io->info('Removed site configuration.');
+
+        return Command::SUCCESS;
     }
 }
