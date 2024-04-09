89a0de8b5194c362597ca889ea892dcba947d529
Filippos Karpouchtsis <filippos@gmk.de>
2024-04-09 17:28:17 +0100

    [REFACTOR] Use addTCAcolumns for Pages TCA

--- local_packages/psi/Configuration/TCA/Overrides/pages.php
+++ local_packages/psi/Configuration/TCA/Overrides/pages.php
@@ -6,19 +6,19 @@
 
 defined('TYPO3') or die('Access denied.');
 
-// Register fields
-$GLOBALS['TCA']['pages']['columns'] = array_replace_recursive(
-$GLOBALS['TCA']['pages']['columns'],
-    [
-        'navigation_icon' => [
+// Register Extra Cols
+$columns = array(
+    'navigation_icon' => [
         'label' => 'Navigation Icon',
         'config' => [
             'type' => 'file',
             'maxitems' => 1,
             'allowed' => 'common-image-types'
         ],
-        ]
     ]
 );
+
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('pages', $columns);
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('pages', 'navigation_icon', '1,3,4', 'after:nav_title');
