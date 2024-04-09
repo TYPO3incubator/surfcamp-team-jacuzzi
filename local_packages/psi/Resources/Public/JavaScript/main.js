55094d15d019d1ca5e9ad6531d72684d75bab84d
Filippos Karpouchtsis <filippos@gmk.de>
2024-04-09 16:17:27 +0100

    [FEATURE] Add Sidebar Animation Script

--- local_packages/psi/Resources/Public/JavaScript/main.js
+++ local_packages/psi/Resources/Public/JavaScript/main.js
@@ -1,2 +1,15 @@
-alert("file compiled successfully");
-//# sourceMappingURL=main.js.map
+// Sidebar Expand/Collapse Functionality
+
+var menuToggle = document.getElementById('menu-toggle');
+var siteContent = document.getElementById('site-content')
+var mainNavigation = document.getElementById('mainnavigation')
+
+menuToggle.addEventListener('click', () => {
+    if (!siteContent.classList.contains('sidebar-collapse')) {
+        siteContent.classList.add('sidebar-collapse')
+        mainNavigation.setAttribute('aria-expanded', 'false');
+    } else {
+        siteContent.classList.remove('sidebar-collapse')
+        mainNavigation.setAttribute('aria-expanded', 'true');
+    }
+})
