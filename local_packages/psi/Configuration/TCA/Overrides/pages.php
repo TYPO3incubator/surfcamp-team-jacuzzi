b6845dc4f3ae11d74caee48c28a151815da67868
Filippos Karpouchtsis <filippos@gmk.de>
2024-04-11 14:37:23 +0100

    [FEATURE] Register News Page Type

--- local_packages/psi/Configuration/TCA/Overrides/pages.php
+++ local_packages/psi/Configuration/TCA/Overrides/pages.php
@@ -18,7 +18,60 @@ $columns = array(
     ]
 );
 
-
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('pages', $columns);
-
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('pages', 'navigation_icon', '1,3,4', 'after:nav_title');
+
+// Add Custom Doktype to Dropdown
+(function () {
+    // SAME as registered in ext_tables.php
+    $customPageDoktype = 3737;
+
+    // Add the new doktype to the page type selector
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem(
+        'pages',
+        'doktype',
+        [
+            'label' => 'News Item / Event',
+            'value' => $customPageDoktype,
+            'icon'  => 'content-news',
+            'group' => 'special',
+        ],
+    );
+})();
+
+// Register Extra News Item Columns
+$columns = [
+    'location' => [
+        'label' => 'Location',
+        'config' => [
+            'type' => 'text',
+            'max' => 511,
+        ]
+    ],
+    'eventdatetime' => [
+        'label' => 'Date of Event',
+        'config' => [
+            'type' => 'datetime',
+            'format' => 'date',
+            'default' => 0,
+        ]
+    ]
+];
+
+// Add Columns to Pages
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns(
+    'pages',
+    $columns
+);
+
+$GLOBALS['TCA']['pages']['palettes']['news']['showitem'] =
+    'location;Event Location,
+    --linebreak--,
+    eventdatetime; Event Datetime
+    ';
+
+$GLOBALS['TCA']['pages']['types'][3737] = [
+    'showitem' =>
+    '--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general, --palette--;;standard, --palette--;;title,--palette--;;news,--div--;LLL:EXT:seo/Resources/Private/Language/locallang_tca.xlf:pages.tabs.seo, --palette--;;seo, --palette--;;robots, --palette--;;canonical, --palette--;;sitemap, --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.tabs.appearance, --palette--;;layout, --palette--;;replace,--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.tabs.resources, --palette--;;media, --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language, --palette--;;language, --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.tabs.access, --palette--;;visibility, --palette--;;access, --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories, categories, '
+
+];
---
+++ local_packages/psi/ext_tables.sql
@@ -0,0 +1,7 @@
+#
+# Table structure for table 'pages'
+#
+CREATE TABLE pages (
+    eventdatetime bigint(20) DEFAULT '0' NOT NULL,
+    location text DEFAULT '' NOT NULL
+);
