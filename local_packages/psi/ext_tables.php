304c30aa7f36ad95fe2386b8feadc6230788f9ae
Oliver Bartsch <bo@cedev.de>
2024-04-11 16:34:24 +0200

    [TASK] Fix doktype registration

--- local_packages/psi/ext_tables.php
+++ local_packages/psi/ext_tables.php
@@ -5,12 +5,9 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 defined('TYPO3') or die();
 
-// Define a new doktype
-$customPageDoktype = 3737;
 // Add page type to system
-$dokTypeRegistry = GeneralUtility::makeInstance(PageDoktypeRegistry::class);
-$dokTypeRegistry->add(
-    $customPageDoktype,
+GeneralUtility::makeInstance(PageDoktypeRegistry::class)->add(
+    \Jacuzzi\Psi\Domain\NewsPage::NEWS_PAGE_DOKTYPE,
     [
         'type' => 'web',
         'allowedTables' => '*',
