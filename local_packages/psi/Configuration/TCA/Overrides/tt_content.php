497830cd8568885ae2b6a3d3937745f18ca97055
fjora.hodo <fjora.hodo@udg.de>
2024-04-11 10:50:06 +0100

    Content Elements

--- local_packages/psi/Configuration/TCA/Overrides/tt_content.php
+++ local_packages/psi/Configuration/TCA/Overrides/tt_content.php
@@ -64,6 +64,8 @@ $GLOBALS['TCA']['tt_content']['types']['image'] = [
     --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
     --palette--;;general,
     --palette--;;headers,media,
+    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:appearance,
+    --palette--;;frames,
     --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
     --palette--;;language, --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
     --palette--;;hidden,
@@ -84,29 +86,28 @@ $GLOBALS['TCA']['tt_content']['types']['image'] = [
 $GLOBALS['TCA']['tt_content']['ctrl']['typeicon_classes']['image'] = 'content-image';
 
 /**
- * PSI TextMedia Element
+ * PSI TextImage Element
  */
 ExtensionManagementUtility::addTcaSelectItem(
     'tt_content',
     'CType',
     [
-        'label' => 'LLL:EXT:psi/Resources/Private/Language/locallang_db.xlf:ctype.textmedia',
-        'value' => 'textmedia',
+        'label' => 'LLL:EXT:psi/Resources/Private/Language/locallang_db.xlf:ctype.textpic',
+        'value' => 'textpic',
         'icon' => 'content-beside-text-img-above-center',
         'group' => 'PSI',
-        'description' => 'LLL:EXT:psi/Resources/Private/Language/locallang_db.xlf:ctype.textmedia.description',
+        'description' => 'LLL:EXT:psi/Resources/Private/Language/locallang_db.xlf:ctype.textpic.description',
     ]
 );
 
-$GLOBALS['TCA']['tt_content']['types']['textmedia'] = [
+$GLOBALS['TCA']['tt_content']['types']['textpic'] = [
     'showitem' => '
     --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
     --palette--;;general,
     --palette--;;headers, bodytext;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:bodytext_formlabel,
     --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:media, assets,
-    --linebreak--, imageorient;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:imageorient_formlabel,
     --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:appearance,
-    --palette--;;,
+    --palette--;;frames,
     --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
     --palette--;;language, --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
     --palette--;;hidden,
@@ -124,7 +125,13 @@ $GLOBALS['TCA']['tt_content']['types']['textmedia'] = [
                 'richtextConfiguration' => 'default'
             ],
         ],
+        'assets' => [
+            'config' => [
+                'type' => 'file',
+                'allowed' => 'jpg,jpeg,png,webp,svg',
+            ],
+        ],
     ],
 ];
 
-$GLOBALS['TCA']['tt_content']['ctrl']['typeicon_classes']['textmedia'] = 'content-beside-text-img-above-center';
+$GLOBALS['TCA']['tt_content']['ctrl']['typeicon_classes']['textpic'] = 'content-beside-text-img-above-center';
