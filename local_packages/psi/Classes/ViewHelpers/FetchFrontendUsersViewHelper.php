6b95474e511f80a1b6f082e45e9b3b77b3a8202c
Oliver Bartsch <bo@cedev.de>
2024-04-12 12:11:09 +0200

    adjust fe user fetching

--- local_packages/psi/Classes/ViewHelpers/FetchFrontendUsersViewHelper.php
+++ local_packages/psi/Classes/ViewHelpers/FetchFrontendUsersViewHelper.php
@@ -5,6 +5,7 @@ namespace Jacuzzi\Psi\ViewHelpers;
 use TYPO3\CMS\Core\Context\Context;
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Frontend\Resource\FileCollector;
 use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
 
@@ -30,29 +31,18 @@ final class FetchFrontendUsersViewHelper extends AbstractViewHelper
 
         $users = $query->fetchAllAssociative();
         $context = GeneralUtility::makeInstance(Context::class);
-        $userId = $context->getPropertyFromAspect('frontend.user', 'id');
+        $userId = (int)$context->getPropertyFromAspect('frontend.user', 'id');
         foreach ($users as $key => $user) {
-            if ($user['uid'] === $userId) {
+            if ((int)$user['uid'] === $userId) {
                 unset($users[$key]);
+                continue;
             }
             if ($user['image'] > 0) {
-                $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('sys_file_reference');
-                $media = $queryBuilder
-                    ->select('file.*')
-                    ->from('sys_file_reference', 'relation')
-                    ->leftJoin('relation', 'sys_file', 'file', 'relation.uid_local = file.uid')
-                    ->where(
-                        $queryBuilder->expr()->eq('relation.uid_foreign', $queryBuilder->createNamedParameter($user['uid']))
-                    )
-                    ->andWhere(
-                        $queryBuilder->expr()->eq('relation.tableNames', $queryBuilder->createNamedParameter('fe_users'))
-                    )
-                    ->andWhere(
-                        $queryBuilder->expr()->eq('relation.deleted', $queryBuilder->createNamedParameter(0))
-                    )
-                    ->executeQuery()
-                    ->fetchAssociative();
-                $users[$key]['media'] = '/fileadmin' . $media['identifier'];
+                $fileCollector = GeneralUtility::makeInstance(FileCollector::class);
+                $fileCollector->addFilesFromRelation('fe_users', 'image', $user);
+                if (count($files = $fileCollector->getFiles()) > 0) {
+                    $users[$key]['media'] = reset($files);
+                }
             }
         }
         return $users;
